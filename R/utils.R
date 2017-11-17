f3b6cf05f1b1560c3ac18d67c38b34ea6df60bb4
daraybusen <dasha-89.89@mail.ru>
2017-11-17 15:13:15 +0100

    Update

--- R/utils.R
+++ R/utils.R
@@ -11,11 +11,10 @@
 #' @author Darya Busen <\email{dasha-89.89@@mail.ru}>
 #'
 #'
-mean = function(height.input = height) {
-# Do checks
-checkmate::assertVector(height.input)
-checkmate::assert(checkmate::checkInteger(height.input, lower=0, min.len=1, any.missing = FALSE, null.ok=FALSE),
-                  checkmate::checkNumeric(height.input, lower=0, min.len=1, any.missing = FALSE, null.ok=FALSE))
-# Return rounded mean
-return(round(base::mean(height.input),digits = 3))
+#' Return the rounded mean
+mean = function(x) {
+  n = length(x)
+  sum = sum(x)
+  rnd = round(x = (1/n) * sum , digits = 3)
+  return(rnd)
 }
